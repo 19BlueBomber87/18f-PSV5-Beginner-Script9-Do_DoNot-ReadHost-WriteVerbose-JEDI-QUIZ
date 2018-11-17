b2e202e5908235063d52036200841718f303cd87
[The_Blue_Bomber] AKA: [MarkK.] AKA [Mark Willis Kruse] <a19mkruse87k@outlook.com>
2018-11-16 16:07:57 -0900

    Add files via upload

---
+++ 18f-PSV5-Beginner-Script9-Do_DoNot-ReadHost-WriteVerbose JEDI QUIZ.ps1
@@ -0,0 +1,116 @@
+﻿ #$= {
+#Write-Verbose -Message $A_YodaByte.M -Verbose *>&1;
+#Write-Verbose -Message $A_YodaByte.A; $A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
+#$A_YodaByte.M; $A_YodaByte.A; $A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
+#Read-Host "Want to see what a Yoda-Byte Looks Like?"
+Function Yoda-Byte
+{
+cls
+Read-Host "[DO OR DO NOT THERE IS NO TRY]
+Press any key to continue:
+But there isn't 'Any' 'AnyKey'"
+$A_YodaByte=[Ordered]@{M="M"; A="A"; Y="Y"; x ="+"; T="T"; H="H"; E="E"; W ="+"; F="F"; O="R"; C="C";E1="E"; I="+"; B1="B"; E2="E";N="+";Y1="Y";O1="O"; U="U"; G="<@:)"}
+$A_YodaByte.values | Out-File C:\Kruse\A_YodatByte.txt
+$A_YodaByte.values | Out-File C:\Kruse\A_YodatByte_1.txt
+$A_YodaByte.values
+Write-Verbose -Message "MayTheForceBeWithYou
+Are You 'Sith'?
+or
+Are You 'JEDI' " -Verbose *>&1
+$Solo = Read-Host "???:"
+$Lando = Get-Content -raw "C:\Kruse\A_YodatByte.txt"
+$Chewy = Get-Content "C:\Kruse\A_YodatByte_1.txt"
+if ($Solo -eq "Jedi" )
+{
+Write-Verbose -Message $Lando -Verbose *>&1
+Write-Verbose -Message "MayTheForceBeWithYou" -Verbose *>&1
+Write-Verbose -Message "$Chewy" -Verbose *>&1
+}
+elseif ($Solo -eq "Sith" )
+{
+Write-Verbose -Message "The Foce Is Not With You" -Verbose *>&1
+}
+elseif ($solo -ne {"Jedi" -and "Sith"} )
+{
+Write-Verbose -Message "You have to choose. Jedi Or Sith" -Verbose *>&1
+$ThereIsAnotherSkyWalker = Read-Host "Try Again?"
+}
+If ($ThereIsAnotherSkyWalker -eq "Yes")
+{
+Write-Verbose -Message "Restarting: Are you a Sith Or Jedi Quiz" -Verbose *>&1
+Read-Host "Press Any Key To Continue"
+Yoda-Byte
+Clear-Variable ThereIsAnotherSkyWalker
+}
+elseif ($ThereIsAnotherSkyWalker -ne "Yes")
+{
+Write-Verbose -Message ""
+cls
+}
+}
+#$A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
+#$YodaByte = {
+#Write-Verbose -Message $A_YodaByte.M -Verbose *>&1;
+#Write-Verbose -Message $A_YodaByte.A; $A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
+#$A_YodaByte.M; $A_YodaByte.A; $A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
+#Read-Host "Want to see what a Yoda-Byte Looks Like?"
+Function Yoda-Byte
+{
+cls
+Read-Host "[DO OR DO NOT THERE IS NO TRY]
+Press any key to continue:
+But there isn't 'Any' 'AnyKey'"
+$A_YodaByte=[Ordered]@{M="M"; A="A"; Y="Y"; x ="+"; T="T"; H="H"; E="E"; W ="+"; F="F"; O= "O"; R = "R"; C="C";E1="E"; I="+"; B1="B"; E2="E";N="+";W1="W";I1="I";T1="T"; H1="H"; Y1="+";Y3="Y";O4="O";U7="U"; G="<@:)"}
+$A_YodaByte.values | Out-File C:\Kruse\A_YodatByte.txt
+$A_YodaByte.values | Out-File C:\Kruse\A_YodatByte_1.txt
+$A_YodaByte.values
+Write-Verbose -Message "MayTheForceBeWithYou
+Are You 'Sith'?
+or
+Are You 'JEDI' " -Verbose *>&1
+$Solo = Read-Host "???:"
+$Lando = Get-Content -raw "C:\Kruse\A_YodatByte.txt"
+$Chewy = Get-Content "C:\Kruse\A_YodatByte_1.txt"
+if ($Solo -eq "Jedi" )
+{
+Write-Verbose -Message $Lando -Verbose *>&1
+Write-Verbose -Message "MayTheForceBeWithYou" -Verbose *>&1
+Write-Verbose -Message "$Chewy" -Verbose *>&1
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+notepad C:\Kruse\A_YodatByte.txt
+notepad C:\Kruse\A_YodatByte_1.txt
+}
+elseif ($Solo -eq "Sith" )
+{
+Write-Verbose -Message "The Foce Is Not With You" -Verbose *>&1
+$ThereIsAnotherSkyWalker = Read-host "Try Again?"
+}
+elseif ($solo -ne {"Jedi" -and "Sith"} )
+{
+Write-Verbose -Message "You have to choose. Jedi Or Sith" -Verbose *>&1
+$ThereIsAnotherSkyWalker = Read-Host "Try Again?"
+}
+If ($ThereIsAnotherSkyWalker -eq "Yes")
+{
+Write-Verbose -Message "Restarting: Are you a Sith Or Jedi Quiz" -Verbose *>&1
+Read-Host "Press Any Key To Continue"
+Yoda-Byte
+}
+else
+{
+Write-Verbose -Message "INVALID_ENTRY_SHUTTING_DOWN" -Verbose *>&1
+Read-Host "Good_BYE_:)
+TFTSP_FWH"
+}
+}
+Yoda-Byte
+#$A_YodaByte.y; $A_YodaByte.x; $A_YodaByte.T; $A_YodaByte.H; $A_YodaByte.E; $A_YodaByte.w; ; $A_YodaByte.F; $A_YodaByte.O; $A_YodaByte.R; $A_YodaByte.C; $A_YodaByte.E2; $A_YodaByte.I; $A_YodaByte.B1; $A_YodaByte.E2; $A_YodaByte.N; $A_YodaByte.Y; $A_YodaByte.O1; $A_YodaByte.U; $A_YodaByte.G}
